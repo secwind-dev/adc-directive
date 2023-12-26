922a5483c3e608ee049a1f499185ea5381955e98
Wisanu Prayoonsuk <secwind.dev@gmail.com>
2023-12-26 21:53:26 +0700

    commit first fnNu

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-export function dcItemIsDuplicate<T>(
+export function dcCheckItemDuplicate<T>(
     items: T[],
     next: (arg: T) => any
-): boolean {
+): number {
+    if (!Array.isArray(items)) return -1
     const mapItems = items.map((item) => next(item))
     const uniqueValues = new Set(mapItems)
 
-    return uniqueValues.size != items.length
+    return uniqueValues.size !== items.length ? 0 : 1
 }
