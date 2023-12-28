4690b7257bf6cde782703996871026cae082fede
Wisanu Prayoonsuk <secwind.dev@gmail.com>
2023-12-28 23:22:14 +0700

    dcRunProcess
    ปรับ logic dcRunProcess ให้สามารถเลือก run ตั้งแต่ index start-end

--- src/fnRun.ts
+++ src/fnRun.ts
@@ -10,16 +10,23 @@
  *  dcRunProcess(items, (item, index) => {
        console.log(item.name)
     },index)
+ *  dcRunProcess(items, (item, index) => {
+       console.log(item.name)
+    },[2,3])
  */
 export function dcRunProcess<T>(
     items: T[],
     next: (args: T, i?: number) => void,
-    index: number = 0
+    _index: number | [number, number] = 0
 ): void {
-    if (index < items.length) {
+    const [index, length] = Array.isArray(_index)
+        ? _index
+        : [_index, items.length]
+    const n = Array.isArray(_index) ? 1 : 0
+    if (index < length + n && index < items.length) {
         const data = items[index]
         next(data, index)
 
-        dcRunProcess(items, next, index + 1)
+        dcRunProcess(items, next, [index + 1, length])
     }
 }
--- src/tests/run.test.ts
+++ src/tests/run.test.ts
@@ -45,10 +45,21 @@ describe('ADC Run of Loop', () => {
     })
     it('dcRunProcess expect Total All Salary', () => {
         let val = 0
-        run.dcRunProcess(USERS, (item, index) => {
-            val = val += item.salary
+        run.dcRunProcess(USERS, (v, index) => {
+            val = val += v.salary
         })
         expect(val).toBe(86500)
+    })
+    it('dcRunProcess เลือก between index expect 2+3+4+5 = 18', () => {
+        let val = 0
+        run.dcRunProcess(
+            [1, 2, 3, 4, 5, 6, 7, 8],
+            (v, index) => {
+                val += v
+            },
+            [2, 5]
+        )
+        expect(val).toBe(18)
     })
 
     it('dcToRandomNumber expect true', () => {
